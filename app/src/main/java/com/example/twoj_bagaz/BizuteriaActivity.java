5b834d7491551b4f4a8cead31de882b54d5876d5
Konrad <konradcrg@gmail.com>
2023-06-08 16:28:10 +0200

    Merge remote-tracking branch 'origin/Update' into edycja klass

--- app/src/main/java/com/example/twoj_bagaz/BizuteriaActivity.java
+++ app/src/main/java/com/example/twoj_bagaz/BizuteriaActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.twoj_bagaz.R;
-
 public class BizuteriaActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/twoj_bagaz/MainActivity.java
+++ app/src/main/java/com/example/twoj_bagaz/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
